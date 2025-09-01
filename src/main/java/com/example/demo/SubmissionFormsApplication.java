1e33f90b0ab7a7b6236cae4075c37dc54993d9ea
nithishreddy1 <nithishreddy307@gmail.com>
2025-09-01 07:11:43 -0400

    new change

--- src/main/java/com/example/demo/SubmissionFormsApplication.java
+++ src/main/java/com/example/demo/SubmissionFormsApplication.java
@@ -15,6 +15,7 @@ public class SubmissionFormsApplication extends SpringBootServletInitializer {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SubmissionFormsApplication.class, args);
+		System.out.println("Hello world");
 	}
 
 }
